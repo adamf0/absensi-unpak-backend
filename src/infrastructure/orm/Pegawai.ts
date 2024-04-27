c8210f0dd0c22a142502254aba7809be87349321
!Github@251423! <studiocode52@gmail.com>
2024-04-27 13:46:38 +0700

    bug

--- src/infrastructure/orm/Pegawai.ts
+++ src/infrastructure/orm/Pegawai.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata"
-import { Entity, Column, OneToOne, PrimaryGeneratedColumn} from "typeorm"
+import { Entity, Column, OneToOne, PrimaryGeneratedColumn, PrimaryColumn} from "typeorm"
 import { Pengguna } from "./Pengguna"
 
 @Entity("n_pribadi")
 export class Pegawai {
 
-    @PrimaryGeneratedColumn()
-    id_n_pribadi: number
+    // @PrimaryGeneratedColumn()
+    // id_n_pribadi: number
 
-    @Column({length: 30, type: "varchar"})
+    @PrimaryColumn({length: 30, type: "varchar"})
+    // @Column({length: 30, type: "varchar"})
     nip: string
 
     @Column({length: 30, type: "varchar"})
