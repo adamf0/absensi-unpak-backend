62d82889422c2c0068952a846e819266ad4a8805
!Github@251423! <studiocode52@gmail.com>
2024-04-27 12:24:17 +0700

    debug

--- src/application/absen/CreateAbsenMasukCommandHandler.ts
+++ src/application/absen/CreateAbsenMasukCommandHandler.ts
@@ -38,13 +38,13 @@ export class CreateAbsenMasukCommandHandler implements ICommandHandler<CreateAbs
     } else{
       throw new Error("invalid CreateAbsenMasukCommand")
     }
-    logger.info({absen:absen})
     if(absen==null) throw new Error(`data absen ${target} pada tanggal ${command.tanggal} tidak ditemukan`)
-    
-    absen.absen_masuk = moment(`${command.tanggal} ${command.absen_masuk}:00`).tz('Asia/Jakarta').format('YYYY-MM-DD HH:mm:ss');
+    const x = moment(`${command.tanggal} ${command.absen_masuk}:00`).tz('Asia/Jakarta').format('YYYY-MM-DD HH:mm:ss')
+
+    absen.absen_masuk = x;
     absen.catatan_telat = command.catatan;
     await _db.getRepository(Absen).save(absen);
-    logger.info({absenInsert:absen})
+    logger.info({absen_masuk:x})
 
     // const application: Application = new Application(
     //   command.guid,
